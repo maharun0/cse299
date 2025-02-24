8db38f854a6b2e8786cbd0c669edc502a411e836
maharun0 <maharun.dev@gmail.com>
2025-02-25 05:11:59 +0600

    added semantic chunker related coes

---
+++ server/BotUtils/Chunker.py
@@ -0,0 +1,23 @@
+""" Text Splitters """
+""" Document -> Splits or Chunks """
+# https://python.langchain.com/docs/concepts/text_splitters/
+
+""" Semantic Chunking """
+# https://python.langchain.com/docs/how_to/semantic-chunker/
+
+# What semantic chunking threshold to choose?
+# https://youtu.be/7_YNeJvcAtM
+
+from langchain_ollama import OllamaEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+
+def semanticChunker(docs):
+    try:
+        embed = OllamaEmbeddings(model="nomic-embed-text")
+        text_splitter = SemanticChunker(embed, breakpoint_threshold_type="standard_deviation")
+        chunkedDocuments = text_splitter.transform_documents(docs)
+        print("Documents chunked semantically.")
+        return chunkedDocuments
+    except Exception as e:
+        print(f"Error semantically chunking documents: {e}")
+
