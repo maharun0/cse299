2ab8a936c31de5624668b0a40fb10ad1b7e93016
maharun0 <maharun.dev@gmail.com>
2025-02-24 16:59:51 +0600

    created BotUtils module for better organization

---
+++ server/BotUtils/DocumentLoader.py
@@ -0,0 +1,11 @@
+""" Document Loaders """
+""" PDF, PPTX, CSV, etc. -> Documents""" 
+# https://python.langchain.com/docs/concepts/document_loaders/
+
+from langchain_community.document_loaders import PyPDFLoader
+
+def loadDocument(file_path) {
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    return docs
+}
